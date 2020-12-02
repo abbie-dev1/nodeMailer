9ff23b47e6d1e6d52a22f100f09bc1e01f97ec17
Obakeng <obakeng.phikiso@gmail.com>
2020-12-02 22:19:32 +0200

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require(cors);
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const route = express.Router();
