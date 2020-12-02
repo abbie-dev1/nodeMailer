bc879f54e8ece4b9fb18b51c9c36cba7ec521edf
Obakeng <obakeng.phikiso@gmail.com>
2020-12-02 21:26:14 +0200

    init

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# nodejs-mailsending
-
-# git clone https://github.com/sudhanshu647/nodejs-mailsending.git
-
 # npm i
 
 change gmail user name and password
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const route = express.Router();
@@ -20,49 +19,21 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: 'youremail@gmail.com',
-        pass: 'xxxxxxxxxx',
+        user: 'lethabosithole01@gmail.com',
+        pass: 'KGANYAam199010',
     },
-    secure: true, // upgrades later with STARTTLS -- change this based on the PORT
+    from: 'lethabosithole01@gmail.com',
+    secure: true,
 });
 
-route.post('/text-mail', (req, res) => {
-    const {to, subject, text } = req.body;
+route.post('/email', (req, res) => {
+    const { to, subject, text, name } = req.body;
     const mailData = {
-        from: 'youremail@gmail.com',
+        from: 'lethabosithole01@gmail.com',
         to: to,
         subject: subject,
         text: text,
-        html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
-    };
-
-    transporter.sendMail(mailData, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        res.status(200).send({ message: "Mail send", message_id: info.messageId });
-    });
-});
-
-
-route.post('/attachments-mail', (req, res) => {
-    const {to, subject, text } = req.body;
-    const mailData = {
-        from: 'youremail@gmail.com',
-        to: to,
-        subject: subject,
-        text: text,
-        html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>',
-        attachments: [
-            {   // file on disk as an attachment
-                filename: 'nodemailer.png',
-                path: 'nodemailer.png'
-            },
-            {   // file on disk as an attachment
-                filename: 'text_file.txt',
-                path: 'text_file.txt'
-            }
-        ]
+        html: `<b>Hey ${name} </b><br> ${text}<br/>`,
     };
 
     transporter.sendMail(mailData, (error, info) => {
